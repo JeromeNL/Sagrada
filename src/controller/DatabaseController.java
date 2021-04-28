2a6182c76651ea9d0da15482ab65e77f0c362951
Jasper van der Veen <jj.vanderveen3@student.avans.nl>
2021-04-28 15:41:36 +0200

    Cleaned code DatabaseController
    Comments toegevoegd voor verduidelijking en onnodige code verwijderd.

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -9,13 +9,18 @@ import java.sql.Statement;
 public class DatabaseController {
 
 	private Connection m_Conn;
-	
+
 	public DatabaseController() {
+		// Setting up database connection
 		if (loadDataBaseDriver("com.mysql.jdbc.Driver")) {
-			makeConnection();			
+			makeConnection();
 		}
-	}	
-	
+	}
+
+	/*
+	 * Method that loads the database driver. Required to execute before
+	 * establishing the connection. Returns true if database driver has been loaded.
+	 */
 	public boolean loadDataBaseDriver(String driverName) {
 		try {
 			Class.forName(driverName);
@@ -24,13 +29,16 @@ public class DatabaseController {
 			return false;
 		}
 	}
-	
+
+	/*
+	 * Method that attempts to establish a connection to the database. Returns true
+	 * if connection is established.
+	 */
 	public boolean makeConnection() {
 		try {
 			m_Conn = DriverManager.getConnection(
 					"jdbc:mysql://databases.aii.avans.nl/jwkwette_db2?" + "user=jwkwette&password=Ab12345");
 		} catch (SQLException ex) {
-			// handle any errors
 			System.out.println("Kan geen verbinding maken met de database. Lees hieronder waarom:");
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
@@ -39,16 +47,17 @@ public class DatabaseController {
 		}
 		return true;
 	}
-	
-/// Methode om een query uit te voeren op de database. Methode returned de result set.
-	
+
+	/*
+	 * Method to execute SELECT statements on the database. Returns: a ResultSet
+	 * object that contains the data produced by the given query; never null
+	 */
 	public ResultSet doQuery(String query) {
-		Statement statement = null;
+		Statement statement;
 		try {
 			statement = m_Conn.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
-//			statement.close();
-			
+
 			return resultSet;
 		} catch (SQLException e) {
 			System.out.println("Something went wrong while executing the following query: " + query);
@@ -56,10 +65,11 @@ public class DatabaseController {
 		}
 		return null;
 	}
-	
+
 	/*
-	 * Methode om INSERT/UPDATE/DELETE statements uit te voeren op de database.
-	 * Returns: aantal geüpdate rows OF 0 als het SQL query is dat niks teruggeeft.
+	 * Method to execute INSERT/UPDATE/DELETE on the database. Returns: either (1)
+	 * the row count for SQL Data Manipulation Language (DML) statementsor (2) 0 for
+	 * SQL statements that return nothing
 	 */
 	public int doUpdateQuery(String query) {
 		Statement statement;
@@ -67,52 +77,10 @@ public class DatabaseController {
 			statement = m_Conn.createStatement();
 			return statement.executeUpdate(query);
 		} catch (SQLException e) {
-			System.out.println("Something went wrong while executing the following insert/update/delete query: " + query);
+			System.out
+					.println("Something went wrong while executing the following insert/update/delete query: " + query);
 			System.out.println(e.getMessage());
 		}
 		return 0;
 	}
-
-	public void doSomeQuering() {
-		System.out.println("Gaat nu een query doen, moment..");
-
-		Statement stmt = null;
-		String query = "SELECT * FROM round";
-		try {
-			stmt = m_Conn.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			while (rs.next()) {
-				String roundID = rs.getString("RoundID");
-				String roundnr = rs.getString(2);
-
-				System.out.println("RoundID: " + roundID + " roundnr: " + roundnr);
-
-			}
-			stmt.close();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-
-//	// eigengemaakte test: vraagt alle kleuren die een 'u' bevatten op uit de DB.
-//	public void printColorsWithU() {
-//		System.out.println("Vraagt alle kleuren met een U op...");
-//		Statement s = null;
-//		String query = "SELECT * FROM color WHERE color LIKE '%u%';";
-//
-//		try {
-//			s = m_Conn.createStatement();
-//			ResultSet rs = s.executeQuery(query);
-//			System.out.println("-----()-----");
-//			while (rs.next()) {
-//				String askTable = rs.getString("color");
-//
-//				System.out.println("Kleuren met een U: " + askTable);
-//			}
-//			s.close();
-//		} catch (SQLException e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
-
 }
