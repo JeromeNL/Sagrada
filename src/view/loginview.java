76e0a69e5d4bcd0b219edd957f6c1b34ad8a3f67
JeromeNL <jw.kwetters@student.avans.nl>
2021-04-30 00:38:04 +0200

    Opzet login/registratie logica
    - In view/loginview.java even een cntrl+shift+f uitgevoerd (daardoor lijkt het misschien wel alsof ik de halve klasse omgebouwd heb; is niet zo)
    
    - In de view/loginview.java Actions toegevoegd aan de knoppen
    
    - In model/RegisterLoginModel.java een start gemaakt aan de login/register logica:
    * Registratie nieuw account naar DB werkt
    * wat voorbereidend werk verricht om verder op te bouwen

--- src/view/loginview.java
+++ src/view/loginview.java
@@ -1,7 +1,11 @@
 package view;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -30,43 +34,47 @@ public class loginview extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		
-		
+
+		model.RegisterLoginModel rlm = new model.RegisterLoginModel();
+
 		InputStream stream = new FileInputStream(".//LoginImg.png");
-	      Image image = new Image(stream);
-	      ImageView imageView = new ImageView();
-	      imageView.setImage(image);
-	      imageView.setX(10);
-	      imageView.setY(10);
-	      imageView.setFitWidth(575);
-	      imageView.setPreserveRatio(true);
-		
+		Image image = new Image(stream);
+		ImageView imageView = new ImageView();
+		imageView.setImage(image);
+		imageView.setX(10);
+		imageView.setY(10);
+		imageView.setFitWidth(575);
+		imageView.setPreserveRatio(true);
+
 		VBox Box = new VBox();
 		HBox SagradaText = new HBox();
 		HBox Indeling = new HBox();
 		HBox knoppen = new HBox(5);
 		VBox tekst = new VBox();
 		VBox Textvakken = new VBox(10);
-	
+
 		Stage loginstage = new Stage();
-		Scene loginscene = new Scene(Box ,Width, Height, Color.rgb(247, 150, 150));
-	
+		Scene loginscene = new Scene(Box, Width, Height, Color.rgb(247, 150, 150));
+
 		TextField tfName = new TextField();
 		PasswordField tfPassword = new PasswordField();
 		Button Login = new Button("  Login  ");
 		Button Register = new Button("Register");
 		Text Sagradatxt = new Text("SAGRADA");
-		
+
 		Box.getChildren().addAll(SagradaText, Indeling);
 		Indeling.getChildren().addAll(Textvakken, imageView);
 		SagradaText.getChildren().addAll(Sagradatxt);
 		Textvakken.getChildren().addAll(tekst, knoppen);
 		tekst.getChildren().addAll(tfName, tfPassword);
 		knoppen.getChildren().addAll(Login, Register);
-		
-		Box.setBackground(new Background(new BackgroundFill(Color.rgb(247, 150, 150), CornerRadii.EMPTY, Insets.EMPTY)));
-		tfName.setBackground(new Background(new BackgroundFill(Color.rgb(247, 150, 150, 0.6), CornerRadii.EMPTY, Insets.EMPTY)));
-		tfPassword.setBackground(new Background(new BackgroundFill(Color.rgb(247, 150, 150, 0.6), CornerRadii.EMPTY, Insets.EMPTY)));
+
+		Box.setBackground(
+				new Background(new BackgroundFill(Color.rgb(247, 150, 150), CornerRadii.EMPTY, Insets.EMPTY)));
+		tfName.setBackground(
+				new Background(new BackgroundFill(Color.rgb(247, 150, 150, 0.6), CornerRadii.EMPTY, Insets.EMPTY)));
+		tfPassword.setBackground(
+				new Background(new BackgroundFill(Color.rgb(247, 150, 150, 0.6), CornerRadii.EMPTY, Insets.EMPTY)));
 		tfName.setPromptText("Username");
 		tfPassword.setPromptText("Password");
 		SagradaText.setAlignment(Pos.CENTER);
@@ -74,49 +82,54 @@ public class loginview extends Application {
 		Textvakken.setAlignment(Pos.CENTER);
 		SagradaText.setMinHeight(100);
 		Textvakken.setMinWidth(400);
-		
-		Sagradatxt.setStyle(
-				"-fx-font: 80 futura;" +
-		
-						"-fx-fill: white;"
-				);
-		
-		tfName.setStyle(
-			"-fx-border-color: white;\r\n" + 
-			"    -fx-border-width: 0 0 3 0;\r\n" + 
-			"    -fx-underline: true;" +
-			"-fx-prompt-text-fill: white;"
-				);
-		tfPassword.setStyle(
-			"-fx-border-color: white;\r\n" + 
-			"    -fx-border-width: 0 0 3 0;\r\n" + 
-			"    -fx-underline: true;" +
-			"-fx-prompt-text-fill: white;"
-							);
-		
-		String IDLE_BUTTON_STYLE = "-fx-border-width: 2;" +
-				"-fx-border-color: white;" +
-				"-fx-background-color: transparent;" +
-				"-fx-font-size: 20;" +
-				"-fx-text-fill: white;";
-		String HOVERED_BUTTON_STYLE = "-fx-border-width: 2;" +
-				"-fx-border-color: white;" +
-				"-fx-background-color:white;" +
-				"-fx-font-size: 20;" +
-				"-fx-text-fill:blue;";
+
+		Sagradatxt.setStyle("-fx-font: 80 futura;" +
+
+				"-fx-fill: white;");
+
+		tfName.setStyle("-fx-border-color: white;\r\n" + "    -fx-border-width: 0 0 3 0;\r\n"
+				+ "    -fx-underline: true;" + "-fx-prompt-text-fill: white;");
+		tfPassword.setStyle("-fx-border-color: white;\r\n" + "    -fx-border-width: 0 0 3 0;\r\n"
+				+ "    -fx-underline: true;" + "-fx-prompt-text-fill: white;");
+
+		String IDLE_BUTTON_STYLE = "-fx-border-width: 2;" + "-fx-border-color: white;"
+				+ "-fx-background-color: transparent;" + "-fx-font-size: 20;" + "-fx-text-fill: white;";
+		String HOVERED_BUTTON_STYLE = "-fx-border-width: 2;" + "-fx-border-color: white;"
+				+ "-fx-background-color:white;" + "-fx-font-size: 20;" + "-fx-text-fill:blue;";
 
 		Register.setStyle(IDLE_BUTTON_STYLE);
 		Register.setOnMouseEntered(e -> Register.setStyle(HOVERED_BUTTON_STYLE));
 		Register.setOnMouseExited(e -> Register.setStyle(IDLE_BUTTON_STYLE));
+
+		// Event after clicking the "Register" button
+		Register.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Register knop test");
+				;
+				rlm.setRegisterUsernameGiven(tfName.getText());
+				rlm.setRegisterPasswordGiven(tfPassword.getText());
+				rlm.registerAccount();
+			}
+		});
+
 		Login.setStyle(IDLE_BUTTON_STYLE);
 		Login.setOnMouseEntered(e -> Login.setStyle(HOVERED_BUTTON_STYLE));
 		Login.setOnMouseExited(e -> Login.setStyle(IDLE_BUTTON_STYLE));
-		
-				
+
+		// Event after clicking the "Login" button
+		Login.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Login knop test");
+				;
+
+			}
+		});
+
 		loginstage.setScene(loginscene);
 		loginstage.show();
-		
-		
+
 	}
 
 }
