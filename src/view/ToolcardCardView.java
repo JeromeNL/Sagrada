008db3dcbc574d0abc1bb770b91a4146ec558263
MandyHoang <mt.hoang@student.avans.nl>
2021-05-04 20:56:03 +0200

    comments toegepast
    Code efficiënter gemaakt en het mapje met afbeelding gefixt.
    
    Co-Authored-By: ImkeLooman <82814741+ImkeLooman@users.noreply.github.com>

--- src/view/ToolcardCardView.java
+++ src/view/ToolcardCardView.java
@@ -15,7 +15,7 @@ import javafx.scene.shape.StrokeType;
 public class ToolcardCardView extends HBox {
 
 	final static Color SAGRADAPINK = Color.rgb(247, 150, 150);
-	private static final String URL_TO_IMAGE = "/images/Group_24.png";
+	static final String URL_TO_IMAGE = "/Group_24.png";
 
 	private int widthCard = 230;
 	private int heightCard = 334;
@@ -34,101 +34,39 @@ public class ToolcardCardView extends HBox {
 	}
 
 	public void addCards() {
-		
+
 		Image group = new Image(getClass().getResource(URL_TO_IMAGE).toString());
 
 		ImageView imageView = new ImageView(group);
 		ImageView imageView2 = new ImageView(group);
 		ImageView imageView3 = new ImageView(group);
-		
-		imageView.setFitHeight(heightCard-30);
-		imageView.setFitWidth(widthCard-30);
-		imageView2.setFitHeight(heightCard-30);
-		imageView2.setFitWidth(widthCard-30);
-		imageView3.setFitHeight(heightCard-30);
-		imageView3.setFitWidth(widthCard-30);
-		
+
+		imageView.setFitHeight(heightCard - 30);
+		imageView.setFitWidth(widthCard - 30);
+		imageView2.setFitHeight(heightCard - 30);
+		imageView2.setFitWidth(widthCard - 30);
+		imageView3.setFitHeight(heightCard - 30);
+		imageView3.setFitWidth(widthCard - 30);
 
 		StackPane card1 = new StackPane();
 		StackPane card2 = new StackPane();
 		StackPane card3 = new StackPane();
-		
-
-		Rectangle r1 = new Rectangle(widthCard, heightCard);
-		r1.setFill(Color.WHITE);
-		r1.setArcWidth(29.0);
-		r1.setArcHeight(29.0);
-		card1.getChildren().addAll(r1, imageView); 
-		
-		r1.hoverProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean show) -> {
-			if(show) {
-				r1.setStroke(Color.LIGHTPINK);
-				r1.setStrokeWidth(4.0);
-				r1.setStrokeType(StrokeType.INSIDE);
-			} else {
-
-				r1.setStrokeWidth(0.0);
-		
-			}
-		});
-		
-		r1.setOnMouseClicked(e->
-			{
-			
-			System.out.println("click card1");
-		});
-
-		Rectangle r2 = new Rectangle(widthCard, heightCard);
-		r2.setFill(Color.WHITE);
-		r2.setArcWidth(29.0);
-		r2.setArcHeight(29.0);
-		card2.getChildren().addAll(r2, imageView2);
-		
-		r2.hoverProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean show) -> {
-			if(show) {
-				r2.setStroke(Color.LIGHTPINK);
-				r2.setStrokeWidth(4.0);
-				r2.setStrokeType(StrokeType.INSIDE);
-			} else {
-
-				r2.setStrokeWidth(0.0);
-		
-			}
-		});
-		
-		r2.setOnMouseClicked(e->
-			{
-			
-			System.out.println("click card2");
-		});
 
 
-		Rectangle r3 = new Rectangle(widthCard, heightCard);
-		r3.setFill(Color.WHITE);
-		r3.setArcWidth(29.0);
-		r3.setArcHeight(29.0);
-		card3.getChildren().addAll(r3, imageView3);
-		r3.hoverProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean show) -> {
-			if(show) {
-				r3.setStroke(Color.LIGHTPINK);
-				r3.setStrokeWidth(4.0);
-				r3.setStrokeType(StrokeType.INSIDE);
-			} else {
 
-				r3.setStrokeWidth(0.0);
+
+
+
+		RectangleCard rectangleCard1 = new RectangleCard(widthCard, heightCard, "card1");
+		RectangleCard rectangleCard2 = new RectangleCard(widthCard, heightCard, "card2");
+		RectangleCard rectangleCard3 = new RectangleCard(widthCard, heightCard, "card3");
 		
-			}
-		});
-		
-		r3.setOnMouseClicked(e->
-			{
-			
-			System.out.println("click card3");
-		});
-		
+		card1.getChildren().addAll(rectangleCard1, imageView);
+		card2.getChildren().addAll(rectangleCard2, imageView2);
+		card3.getChildren().addAll(rectangleCard3, imageView3);
+
 		getChildren().addAll(card1, card2, card3);
 
 	}
-
 
 }
