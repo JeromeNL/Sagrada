bbfe356df779fca53d7d53cb6e375232115afc72
DanideRooij <df.derooij@student.avans.nl>
2021-05-05 16:18:55 +0200

    Aanpassing login/registerView
    Er is een Rectangle toegevoegd voor een colorBurn(wordt nog aangepast door Janique), verder hebben we het aangepast zodat het geen mainclass meer is en zijn de benamingen nagelopen.

--- src/view/MainScene.java
+++ src/view/MainScene.java
@@ -3,30 +3,27 @@ package view;
 import controller.MainController;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import model.Die;
-import model.DiesInSupply;
 import model.Game;
-import model.GameColor;
 import model.Player;
 
 public class MainScene extends Scene {
 
-///////////////////////////////////////////////////////////////// Constructor
-	
 	private MainController mainController;
 
 	public MainScene(MainController mainController) {
 		super(new Pane(), 1280, 720);
+
 		this.mainController = mainController;
-		
-		LobbyView lobbyView = new LobbyView(); 
+
+		LobbyView lobbyView = new LobbyView();
 		setRoot(lobbyView);
 	}
-	
+
 	// show gameview of player of game with nr 1,2,3 or 4.
 	public void showGame(Game game, int playerNR) {
 		Player playerShownOnScreen = game.getPlayers().get(playerNR); // creator of the game
-		GameView gameView = new GameView(game, playerShownOnScreen); 
+		GameView gameView = new GameView(game, playerShownOnScreen);
 		setRoot(gameView);
+
 	}
 }
