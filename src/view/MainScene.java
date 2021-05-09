50f613572b07e9006ea270025ce4e279b41ab0f1
DanideRooij <df.derooij@student.avans.nl>
2021-05-09 17:34:04 +0200

    PlayerList
    Playerlist bijna af, is alleen kleine change op nodig. kom ik in de combiles op terug.

--- src/view/MainScene.java
+++ src/view/MainScene.java
@@ -14,18 +14,20 @@ public class MainScene extends Scene {
 		super(new Pane(), 1280, 720);
 		
 		// aanmaken onderstaande diesupply moet later in main controller gebeuren.
-		DiesInSupply diesInSupply = new DiesInSupply();
-		diesInSupply.addDie(new Die(Color.MEDIUMPURPLE, 3, 1));
-		diesInSupply.addDie(new Die(Color.LIGHTYELLOW, 2, 2));
-		diesInSupply.addDie(new Die(Color.LIGHTBLUE, 6, 3));
-		diesInSupply.addDie(new Die(Color.INDIANRED, 1, 4));
-		diesInSupply.addDie(new Die(Color.LIGHTGREEN, 5, 5));
-
-		GameView gameView = new GameView(diesInSupply); 
-		setRoot(gameView);
-		
+//		DiesInSupply diesInSupply = new DiesInSupply();
+//		diesInSupply.addDie(new Die(Color.MEDIUMPURPLE, 3, 1));
+//		diesInSupply.addDie(new Die(Color.LIGHTYELLOW, 2, 2));
+//		diesInSupply.addDie(new Die(Color.LIGHTBLUE, 6, 3));
+//		diesInSupply.addDie(new Die(Color.INDIANRED, 1, 4));
+//		diesInSupply.addDie(new Die(Color.LIGHTGREEN, 5, 5));
+//
+//		GameView gameView = new GameView(diesInSupply); 
+//		setRoot(gameView);
+//		
 //		LobbyView lobbyView = new LobbyView(); 
 //		setRoot(lobbyView);
 		
+		PlayerListView SpelersView = new PlayerListView();
+		setRoot(SpelersView);
 	}
 }
