5ff3451e2fc2448f65a195e26800ff8bbbfcf2a8
Jasper <48756416+StickyEagle@users.noreply.github.com>
2021-05-10 19:56:46 +0200

    Comment vertaald in Engels
    a.d.h.v. feedback Janique

--- src/model/Round.java
+++ src/model/Round.java
@@ -1,18 +1,15 @@
 package model;
 
 public class Round {
-	
-	/*
-	 * Er zijn 20 entries in de round lookuptable. Deze tabel splitst elke ronde op
-	 * in 2 roundIDs: een met de klok mee en een tegen de klok in gedeelte. roundID
-	 * 1 heeft roundnr 1 en clockwise 1: roundID 1 is dus ronde 1 met de klok mee.
-	 * roundID 12 is dus ronde 6 tegen de klok in.
-	 */
-	
+
+//	There are 20 entries in the round lookuptable. This table divides every round in 2 roundIDs:
+//	one clockwise and the other against the clock. roundID 1 has roundnr 1 en clockwise 1. So roundID 1 is
+//	round 1 clockwise. RoundID 2 is round 1 against the clock. Round 12 is round 6 against the clock.
+
 	private int roundID; // 1 - 20
 	private int roundnr; // 1 - 10
 	private boolean clockwise;
-	
+
 	public Round(int roundID, int roundnr, boolean clockwise) {
 		this.roundID = roundID;
 		this.roundnr = roundnr;
