65f2c0e08fba516c0afd0403a543b01f238da68c
jjanique09 <Janique>
2021-05-13 01:02:10 +0200

    lobbuserView

--- src/view/LobbyUserView.java
+++ src/view/LobbyUserView.java
@@ -1,9 +1,12 @@
 package view;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -21,11 +24,42 @@ private LobbyLineView lobbyLineView;
 private LobbyWaitLineView lobbyWaitLineView;
 private LobbyViewInGame lobbyViewInGame;
 private DropShadow dropShadow;
-
+TextField tfName;
 ///////////////////////////////////////////////////////////////// Constructor	
 	
 	public LobbyUserView() {
-
+		tfName = new TextField();
+		tfName.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(110), Insets.EMPTY)));
+		tfName.setPromptText("Username");
+		tfName.setStyle("-fx-border-color: pink;\r\n" + " -fx-border-width: 0 0 3 0;\r\n"
+ 				+ "    -fx-underline: true;" + "-fx-prompt-text-fill: pink;");
+		tfName.setMaxWidth(400);
+		tfName.setMinHeight(50);
+		tfName.setFont(new Font("Arial", 20));
+		
+		Label addedUser1 = new Label("Janique is toegevoegd"); 
+		Label addedUser2 = new Label("Joram is toegevoegd"); 
+		Label addedUser3 = new Label("Jasper is toegevoegd"); 
+		
+		addedUser1.setTranslateX(-135);
+		addedUser1.setTranslateY(60);
+		addedUser1.setTextFill(Color.LIGHTBLUE);
+		
+		addedUser2.setTranslateX(-137);
+		addedUser2.setTranslateY(100);
+		addedUser2.setTextFill(Color.LIGHTBLUE);
+		
+		addedUser3.setTranslateX(-135);
+		addedUser3.setTranslateY(140);
+		addedUser3.setTextFill(Color.LIGHTBLUE);
+		
+		
+		
+	
+		
+		
+		
+		
 		lobbyLineView = new LobbyLineView();
 		lobbyWaitLineView = new LobbyWaitLineView();
 		lobbyViewInGame = new LobbyViewInGame();
@@ -62,7 +96,7 @@ private DropShadow dropShadow;
 	
 			
 		
-this.getChildren().addAll(panel,availableGamesTitle);
+this.getChildren().addAll(panel,availableGamesTitle,tfName,addedUser1,addedUser2,addedUser3);
 setBackground(new Background(new BackgroundFill(Color.PINK, null, null)));
 
 		
