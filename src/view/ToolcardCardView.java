b9bd6d48017906568d1fac73034691de9eb7c9dd
ImkeLooman <isa.looman@student.avans.nl>
2021-06-05 22:09:33 +0100

    'Terug' button setOnMouseEntered en setOnMouseExited
    'Terug' button veranderd van kleur als je er met je muis in/uit gaat.

--- src/view/ToolcardCardView.java
+++ src/view/ToolcardCardView.java
@@ -8,8 +8,6 @@ import imageChooser.PublicObjectiveCardImage;
 import imageChooser.ToolcardCardImage;
 import javafx.beans.value.ObservableValue;
 
-
-
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -28,20 +26,19 @@ public class ToolcardCardView extends Pane {
 
 	final static Color SAGRADAPINK = Color.rgb(247, 150, 150);
 	static final String URL_TO_IMAGE = "/Group_24.png";
-	RectangleCard rectangleCard1 ;
+	RectangleCard rectangleCard1;
 	private int widthCard = 230;
 	private int heightCard = 334;
 	private DatabaseController dbController;
 	private MainController mainController;
 	private Rectangle rect;
-	private int X=135;
+	private int X = 135;
 	private StackPane card;
 	private Label backButtonText;
 
-
 	public ToolcardCardView(DatabaseController dbController, MainController mainController) {
 		super();
-		
+
 		rect = new Rectangle();
 		rect.setWidth(200);
 		rect.setHeight(60);
@@ -52,21 +49,31 @@ public class ToolcardCardView extends Pane {
 		rect.setArcWidth(50);
 		rect.setStrokeWidth(5);
 		rect.setStroke(Color.WHITE);
-		
+
 		backButtonText = new Label("Terug");
 		backButtonText.setTextFill(Color.WHITE);
 		backButtonText.setFont(new Font("Arial", 15));
 		backButtonText.setStyle("-fx-font-weight: bold");
 		backButtonText.setTranslateY(620);
 		backButtonText.setTranslateX(1015);
-		 
-		 rect.setOnMouseClicked(e -> {System.out.println("button is pressed");
-		 mainController.showGameLoggedInPlayer();
-		 });
-		
+
+		rect.setOnMouseClicked(e -> {
+			System.out.println("button is pressed");
+			mainController.showGameLoggedInPlayer();
+		});
+
+		rect.setOnMouseEntered(e -> {
+			rect.setFill(Color.WHITE);
+			backButtonText.setTextFill(SAGRADAPINK);
+		});
+
+		rect.setOnMouseExited(e -> {
+			rect.setFill(SAGRADAPINK);
+			backButtonText.setTextFill(Color.WHITE);
+		});
+
 		this.dbController = dbController;
 		this.mainController = mainController;
-	
 
 		setBackground(new Background(new BackgroundFill(SAGRADAPINK, null, null)));
 
@@ -77,33 +84,23 @@ public class ToolcardCardView extends Pane {
 
 	public void addCards() {
 
-
-
-		
 		ArrayList<Integer> ids = dbController.getToolcardIDs(55);
 
-		//ArrayList<Integer> ids = dbController.getToolcardIDs(mainController.getCurrentGame().getIdGame());
+		// ArrayList<Integer> ids =
+		// dbController.getToolcardIDs(mainController.getCurrentGame().getIdGame());
 
 		for (Integer id : ids) {
 			StackPane card = new StackPane();
 			ToolcardCardImage toolcardCardImage = new ToolcardCardImage(id);
 			toolcardCardImage.setTranslateX(X);
 			toolcardCardImage.setTranslateY(200);
-			X=X+350;
+			X = X + 350;
 			card.getChildren().add(toolcardCardImage);
 			getChildren().add(card);
 		}
-		
+
 		getChildren().add(rect);
 		getChildren().add(backButtonText);
 
 	}
 }
-
-
-
-
-
-
-
-
